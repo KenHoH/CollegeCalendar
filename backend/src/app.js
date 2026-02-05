9b21a2ce43d068c7b158c40fb03e75174c640636
Kenneth Maximillian Pranata <MX25-1@slc.net>
2026-02-05 18:41:54 +0700

    feat: creating event and reading lms done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -75,6 +76,43 @@ app.get('/auth/callback', async (req, res) => {
     res.status(500).send('Authentication failed');
   }
 });
+
+app.get('/college', async (req, res) => {
+  const postData = {
+    "roleActivity": [
+      {
+        "name": "Student",
+        "userCode": "2802397640",
+        "roleId": "4bcb81bd-46a8-4a09-a923-5e812cb7007b",
+        "roleType": "student",
+        "roleOrganizationId": "437a8c7c-d8fa-4bdd-b920-eaeb965ea146",
+        "academicCareer": "RS1",
+        "institution": "BNS01",
+        "isPrimary": true,
+        "isActive": true
+      }
+    ]
+  }
+
+  try {
+    const response = await axios.post('https://func-bm7-schedule-prod.azurewebsites.net/api/Schedule/Month-v1/2026-2-1', postData ,{
+      headers: {
+        "Authorization":"Bearer eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImQ1MTc4ZmQzLThhNzQtNDEwMy1hNzcxLTNjNjVmYWQwYmQ2MiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJLRU5ORVRIIE1BWElNSUxMSUFOIFBSQU5BVEEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJrZW5uZXRoLnByYW5hdGFAYmludXMuYWMuaWQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsiQk4xMjUyNjIzOTEiLCJHdWVzdCJdLCJuYmYiOjE3NzAyNzA4ODcsImV4cCI6MTc3MDM1NzI4NywiaXNzIjoiQmludXNTZXJ2aWNlcyIsImF1ZCI6Ik5leHVzLklkZW50aXR5U2VydmljZSJ9.RkGmHzwrxUDbjLnjiLvVeiWgS3oQppttI6Q1xlUnau3iAZk_uXBYUQBDr4ra5hpGKzIEb9EJAhZYGxl-pUjAmccTAZWwThyhtsw9eNlvZntmbqHs5TaqH7SS1YKJVZAJ7-NXkbSBjgogea8MKWxVLlfTl8J7K3CaSS3S9mz2O74",
+        "academicCareer":"RS1",
+        "institution":"BNS01",
+        "rOId":"437a8c7c-d8fa-4bdd-b920-eaeb965ea146",
+        "roleId":"4bcb81bd-46a8-4a09-a923-5e812cb7007b",
+        "roleName":"Student",
+      }, 
+      timeout: 10000 
+    })
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: 'Request failed' });
+  }
+
+})
 
 app.get('/events', async (req, res) => {
   try {
